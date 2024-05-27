2c1b391191816ac144a8c097b895874daa58425b
Eduardo Campili <eduardo.campili@gmail.com>
2024-05-27 09:55:49 -0300

    se arreglo el tiempo de ttl

--- middlewares/cache.js
+++ middlewares/cache.js
@@ -4,7 +4,7 @@ const cacheOptions = {
   // Namespace used to prevent cache conflicts, must be alphanumeric
   namespace: "expresscache",
   // Store cache entries for 1 minute (can also pass milliseconds e.g 60000)
-  defaultTtl: "15 minute",
+  defaultTtl: "30 minutes",
   statusCodeExpires: {
     400: "5 minutes",
     500: 0,
